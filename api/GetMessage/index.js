29ac24396461a0459dd784d1ed93023b0ecedd98
Ricardo Silva <ricardo.franco.silva@outlook.com>
2020-12-05 00:04:18 +0000

    Testing await

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,3 +1,6 @@
+// https://www.youtube.com/watch?v=gWEYfyLu1ew
+// https://docs.microsoft.com/en-us/azure/static-web-apps/getting-started?tabs=vanilla-javascript
+
 module.exports = async function (context, req) {
     context.res = {
         // status: 200, /* Defaults to 200 */
